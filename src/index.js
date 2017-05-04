40ffefc3d23c00ecf83ba21c3a5db1a08cc7da03
wsavd <tuve@rootfest.net>
2017-05-04 04:50:13 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ import Home from './components/home'
 import Item0 from './components/item0'
 import Item1 from './components/item1'
 
+
 ReactDOM.render(
-  <Router>
+  <Router basename="/joonline">
     <App>
       <Route exact path="/" component={Home}/>
       <Route path="/item0" component={Item0}/>

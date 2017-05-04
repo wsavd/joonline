517cf3839968501f669066aaae88fb850e485c08
wsavd <tuve@rootfest.net>
2017-05-04 18:59:29 +0300

    work

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
+import VideoListItem from '../components/video_list_item'
 
-{/*почему здесь класс а не функция?*/}
-class VideoList extends Component{
-    render() {
-        return (
-        <div>
-            {
-                this.props.videos.length
-            }
-        </div>
-        )
-    }
-}
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+};
 
 export default VideoList;

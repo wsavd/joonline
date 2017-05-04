143bce9892b12da360a649cd69eeb841aa2081e3
wsavd <tuve@rootfest.net>
2017-05-04 06:32:29 +0300

    simple output of SearchBar component

---
+++ src/components/searchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        Search bar here
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom';
 
 
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import App from './components/app'
 import Home from './components/home'
 import Item0 from './components/item0'
 import Item1 from './components/item1'
+import SearchBar from './components/searchBar'
 
 
 ReactDOM.render(
@@ -18,6 +19,7 @@ ReactDOM.render(
       <Route exact path="/" component={Home}/>
       <Route path="/item0" component={Item0}/>
       <Route path="/item1" component={Item1}/>
+      <SearchBar />
     </App>  
   </Router>,
   document.getElementById('root')
